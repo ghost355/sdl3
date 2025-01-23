86a5e962d64f59d14960616c1a963b37e5b251a1
P. Pavlov <kx355@mail.ru>
2025-01-23 16:50:55 +0300

    update style

--- src/quit.c
+++ src/quit.c
@@ -1,12 +1,12 @@
 #include "../include/quit.h"
-#include "../include/data_types.h"
+
 #include <stdlib.h>
 
-void quit(void* appstate, SDL_AppResult result)
-{
+#include "../include/data_types.h"
 
-    AppData* app_data = appstate;
-    free(app_data);
-    SDL_DestroyRenderer(app_data->renderer);
-    SDL_DestroyWindow(app_data->window);
+void quit(void* appstate, SDL_AppResult result) {
+  AppData* app_data = appstate;
+  SDL_DestroyRenderer(app_data->renderer);
+  SDL_DestroyWindow(app_data->window);
+  free(app_data);
 }
--- src/update.c
+++ src/update.c
@@ -1,9 +1,9 @@
 #include "../include/update.h"
+
 #include "../include/data_types.h"
 
-SDL_AppResult update(void* appstate)
-{
-    AppData* app_data = appstate;
-    app_data->score++;
-    return SDL_APP_CONTINUE;
+SDL_AppResult update(void* appstate) {
+  AppData* app_data = appstate;
+  app_data->score++;
+  return SDL_APP_CONTINUE;
 }
